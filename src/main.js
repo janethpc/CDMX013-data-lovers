2bd60aeb58378320854d32568d313c8c8cec7b14
janethpc <108207854+janethpc@users.noreply.github.com>
2022-08-07 09:53:40 -0500

    ejemplos de galeria
    empleo dos maneras de agrupar las imagenes para la elaboracion de la galeria, la primera opciones mas viable para implementar lightbox

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from 'C:\Users\bertha_janeth\Documents\GitHub\DATA LOVERS\CDMX013-data-lovers\src\data\ghibli\ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
