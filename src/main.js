ae1ff2ea20945b9acf07543324c302036df13004
ana-lopez <anitalopez@192.168.1.9>
2022-08-08 12:45:39 -0500

    Agrega banner y un metodo para mostrar data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
-import { example } from './data.js';
+
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(data);
+
+fetch("./data/ghibli/ghibli.json")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(ghibli) {
+       // console.log(ghibli.films[2].title);
+       let data1="";
+       ghibli.films.map((value)=>{
+        data1+=`<section class="movies">
+        <img src="${value.poster}" alt="poster" class="poster">
+        <h2 class="title">${value.title}</h2>
+        <p>${value.release_date}</p>
+      </section>`;
+       });
+       document.getElementById("all_movies").innerHTML=data1;
+        
+    }).catch((error)=>{
+        console.log(error)
+    })
