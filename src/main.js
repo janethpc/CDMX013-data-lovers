3fa99c83832101971544120dff4ae098ec22563b
janethpc <108207854+janethpc@users.noreply.github.com>
2022-08-08 13:52:50 -0500

    manejo de data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//import { cleanghibli, } from './data.js';
+
+
+import data from './data/ghibli/ghibli.js';
+console.log(data);
+
+let data1="";
+       data.films.map((value)=>{
+        data1+=`<section class="movies">
+        <img src="${value.poster}" alt="poster" class="poster">
+        <h2 class="title">${value.title}</h2>
+        <p>${value.release_date}</p>
+      </section>`;
+       });
+       document.getElementById("all_movies").innerHTML=data1;
+
+
