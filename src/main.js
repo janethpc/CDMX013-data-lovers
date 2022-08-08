a39a084d824c1d0282947840d9bd32e01b963947
ana-lopez <anitalopez@192.168.1.9>
2022-08-08 14:30:53 -0500

    agregar banner y estilos css

--- src/main.js
+++ src/main.js
@@ -1,19 +1,11 @@
+//import { cleanghibli, } from './data.js';
 
-//import { example } from './data.js';
-// import data from './data/lol/lol.js';
-//import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-//console.log(data);
+import data from './data/ghibli/ghibli.js';
+console.log(data);
 
-fetch("./data/ghibli/ghibli.json")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(ghibli) {
-       // console.log(ghibli.films[2].title);
-       let data1="";
-       ghibli.films.map((value)=>{
+let data1="";
+       data.films.map((value)=>{
         data1+=`<section class="movies">
         <img src="${value.poster}" alt="poster" class="poster">
         <h2 class="title">${value.title}</h2>
@@ -21,7 +13,3 @@ fetch("./data/ghibli/ghibli.json")
       </section>`;
        });
        document.getElementById("all_movies").innerHTML=data1;
-        
-    }).catch((error)=>{
-        console.log(error)
-    })
