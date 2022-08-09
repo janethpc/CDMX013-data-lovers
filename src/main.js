00d3673d2f2fddcf213731636003532844ebb7a3
janethpc <108207854+janethpc@users.noreply.github.com>
2022-08-08 21:38:55 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,12 +5,12 @@
 import data from './data/ghibli/ghibli.js';
 console.log(data);
 
-let data1="";
-       data.films.map((value)=>{
-        data1+=`<section class="movies">
+let data1 = "";
+data.films.map((value) => {
+  data1 += `<section class="movies">
         <img src="${value.poster}" alt="poster" class="poster">
         <h2 class="title">${value.title}</h2>
         <p>${value.release_date}</p>
       </section>`;
-       });
-       document.getElementById("all_movies").innerHTML=data1;
+});
+document.getElementById("all_movies").innerHTML = data1;
