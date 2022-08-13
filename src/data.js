960fa4322be3db28d7079ef26b32a4b2845621f8
janethpc <108207854+janethpc@users.noreply.github.com>
2022-08-12 23:18:31 -0500

    pruebas de filtrado

--- src/data.js
+++ src/data.js
@@ -1,10 +1,18 @@
-// estas funciones son de ejemplo
-
-export const escribirMensajeConsola =
 
 
+//};
+  
 
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+
+
+  //document.getElementById("all_movies").innerHTML = data_fantasyFilms;
+    //console.log(data_fantasyFilms);
+  
+
+
+
+
+//export const anotherExample = () => {
+  //return 'OMG';
+//};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 
-//import { cleanghibli, } from './data.js';
-//import { cleanghibli, } from './data.js';
+//import {data_fantasy } from './data.js';
+//console.log(data_fantasy);
 
 import data from './data/ghibli/ghibli.js';
 console.log(data);
@@ -15,8 +15,30 @@ data.films.map((value) => {
 });
 document.getElementById("all_movies").innerHTML = data1;
 
+const filtered_resources = data.films.filter(function(data){
+  return data.genre == 'fantasy';
+});
+console.log(filtered_resources);
+
+let data_filmsfantasy= "";
+filtered_resources.map((value) => {
+  data_filmsfantasy += `<section class="genre_fantasy">
+  <img src="${value.poster}" alt="poster" class="poster">
+  <h2 class="title">${value.title}</h2>
+  <p>${value.release_date}</p>
+</section>`;
+});
+document.getElementById("all_fantasy").innerHTML = data_filmsfantasy;
+
+
+
+ 
+
+
+
 var a_parent =  document.querySelectorAll(".a_parent");
 var dd_menu_a = document.querySelectorAll(".dd_menu_a");
+var dd_menu_a_spam = document.querySelectorAll(".dd_menu_a_spam");
 
 a_parent.forEach(function(aitem){
 
@@ -39,4 +61,14 @@ dd_menu_a.forEach(function(dd_menu_item){
 			})
 			dd_menu_item.classList.add("active");
 		})
+})
+
+dd_menu_a_spam.forEach(function(textem){
+
+  textem.addEventListener("click", function(){
+    dd_menu_a_spam.forEach(function(textem){
+      textem.classsection.add("active");
+    })
+    dd_menu_a_spam.classmain.remove("active");
+  })
 })
