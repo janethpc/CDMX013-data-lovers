aaedf5c930defe57c21a8fb3c0e94f23845ee01e
ana-lopez <anitalopez@192.168.1.9>
2022-08-23 12:54:03 -0500

    agrega test a las funciones de filtrado

--- src/data.js
+++ src/data.js
@@ -1,15 +1,15 @@
-export function films_fantasy(jsonData, category) {
-  return jsonData.films.filter(function (data) {
-    return data.genre == category;
-  });
-}
-
 export function films_genders(jsonData) {   //funcion por medio del cual se aplica metodo filter para que me regrese un array que contenga
   let genders_list = [];                       //solo los generos contenidos en el objeto main.js:18-28;
   return jsonData.films.filter(function (data) {
     if (!genders_list.includes(data.genre)) {
       return genders_list.push(data.genre);
     }
+  });
+}
+
+export function filterByCategory(jsonData, category) {
+  return jsonData.films.filter(function (data) {
+    return data.genre == category;
   });
 }
 
@@ -20,53 +20,53 @@ export function dateFilter(data, minYear, maxYear) { //función recibe la data, 
   return filterYear;
 }
 
-export function orderAscending(data){
-  let compareNames = data.sort((a,b)=> {
-    if (a.title < b.title){
+export function ascendingOrder(data) {
+  let compareNames = data.sort((a, b) => {
+    if (a.title < b.title) {
       return -1
     }
-    if (a.title > b.title){
+    if (a.title > b.title) {
       return 1
     }
     return 0
-  }) 
+  })
   return compareNames;
-  } 
+}
 
-  export function orderDescending(data){
-    let backwards = '';
-    return data.reverse(backwards);
-    }
-  
-export function ascending_years(jsonData){
-      let recentMore= jsonData.sort((a, b) => {
-        if (a.release_date > b.release_date){
-          return -1
-        }
-        if(a.release_date < b.release_date) {
-          return 1
-        }
-      })
-      return recentMore;
-    };
+export function descendingOrder(data) {
+  let backwards = '';
+  return data.reverse(backwards);
+}
 
-export function descendingYears(jsonData){
-  let old= jsonData.sort((a, b) => {
-    if (a.release_date < b.release_date){
+export function ascendingYears(jsonData) {
+  let recentMore = jsonData.sort((a, b) => {
+    if (a.release_date > b.release_date) {
       return -1
     }
-    if(a.release_date > b.release_date) {
+    if (a.release_date < b.release_date) {
+      return 1
+    }
+  })
+  return recentMore;
+}
+
+export function descendingYears(jsonData) {
+  let old = jsonData.sort((a, b) => {
+    if (a.release_date < b.release_date) {
+      return -1
+    }
+    if (a.release_date > b.release_date) {
       return 1
     }
   })
   return old;
-};
+}
 
 
 
 
-  
-  
+
+
 
 
 
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,2 +1,36 @@
+import data from '../src/data/ghibli/ghibli.js';
+import { filterByCategory, dateFilter, ascendingOrder } from '../src/data.js';
 
+describe('films',() => {
+    it ('films by genres', () => {
+        const dataGhibli = data;
+        const category = "adventure";
+        //GIVEN THE USER WANTS TO FILTER MOVIES BY GENRE
+        const resolved = filterByCategory(dataGhibli,category);
+        //WHEN  USERS FILTER BY ADVENTURE 
+        //THEN IT WILL DISPLAY 6 FILMS
+        expect(resolved.length).toEqual(6);
+    })
+})
 
+describe('dateFilter',() => {
+    it('filter films by date release between 1988 to 1989',() => {
+        const solved = dateFilter(data.films, 1988 , 1989);
+        expect(solved.length).toEqual(3);
+       //expect(dateFilter(data.films, 1988 , 1989)).toEqual([{release_date: 1989}, {release_date: 1988}]);  
+    })
+})
+
+describe('ascendingOrder',() => {
+    it('order films by title A to Z',() => {
+        const orderMovies = ascendingOrder([data.films[0],data.films[19]])
+        expect(orderMovies).toEqual([data.films[0],data.films[19]])
+        
+    })
+})
+
+// describe('descendingYears', () => {
+//     it('order descending films by year', () => {
+
+//     })
+// })
